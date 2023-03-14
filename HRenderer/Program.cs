9c4fa6e9a725786c1266a26bd7d469f5914a40e5
honmono <1099263878@qq.com>
2023-03-15 01:07:14 +0800

    feat: 修改平台

--- HRenderer/Program.cs
+++ HRenderer/Program.cs
@@ -1,29 +1,15 @@
-﻿using System;
+﻿// See https://aka.ms/new-console-template for more information
+
 using HRenderer.Core;
 using HRenderer.DrawTriangle;
 
-namespace HRenderer {
-    internal class Program {
-        public static void Main(string[] args) {
-            Console.WriteLine("hello world");
-            
-            Program.DoRender();
-        }
+Console.WriteLine("Hello, World!");
 
-        /**
-         * 渲染测试
-         */
-        public static void DoRender() {
-            var renderer = new Renderer(500, 500);
-            renderer.materials.Add(Program.GetMaterial());
-            renderer.Render();
-        }
+var texture = new Texture("./Assets/001.jpg");
+var mesh = new SpriteMesh();
+var shader = new SpriteShader();
+var material = new Material(texture, mesh, shader);
 
-        public static Material GetMaterial() {
-            var texture = new Texture("./Assets/001.jpg");
-            var mesh = new SpriteMesh();
-            var shader = new SpriteShader();
-            return new Material(texture, mesh, shader);
-        }
-    }
-}
+var renderer = new Renderer(500, 500);
+renderer.materials.Add(material);
+renderer.Render();
--- HRenderer/ReadMe.md
+++
@@ -1,4 +0,0 @@
-
-Vertex Shader 输出在 clip space (裁剪空间)
-
-(Vertex Shader) => Clip Space => (透视除法) => NDC => (视口变换) => Window Space => (Fragment Shader)
